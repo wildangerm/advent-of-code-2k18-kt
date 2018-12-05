f25d465a3e93a77c3e022c291b55eb4150135e85
Martin Wildanger <wildanger.m@gmail.com>
2018-12-05 17:16:00 +0100

    Removed Eclipse settings and bin folder

--- bin/advent_of_code_2k18_kt/day3/Claim.kt
+++
@@ -1,5 +0,0 @@
-package advent_of_code_2k18_kt.day3
-
-class Claim(val id: Int,val left: Int,val top:Int,val width:Int,val height:Int){
-	
-}
--- bin/advent_of_code_2k18_kt/day4/Activity.kt
+++
@@ -1,7 +0,0 @@
-package advent_of_code_2k18_kt.day4
-
-enum class Activity {
-	BEGIN,
-	ASLEEP,
-	WAKE_UP
-}
